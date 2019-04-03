fc0c25cc232e686dbd3e9bc41adb5e6fc21fece6
yanghx <yanghx_mail@163.com>
2019-04-03 23:22:56 +0800

    初始化rabbitmq-spring

---
+++ rabbitmq-spring/src/main/java/com/yanghx/rabbitmqspring/RabbitmqSpringApplication.java
@@ -0,0 +1,16 @@
+package com.yanghx.rabbitmqspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangheng
+ */
+@SpringBootApplication
+public class RabbitmqSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringApplication.class, args);
+    }
+
+}
---
+++ rabbitmq-spring/src/test/java/com/yanghx/rabbitmqspring/RabbitmqSpringApplicationTests.java
@@ -0,0 +1,16 @@
+package com.yanghx.rabbitmqspring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqSpringApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
