d2c01e7ba9170dc5d9e059369e5b1ef377554763
yanghx <yanghx_mail@163.com>
2019-04-06 15:45:36 +0800

    springboot rabbitmq 生产端项目搭建

---
+++ rabbitmq-springboot-producer/src/main/java/com/yanghx/rabbitmqspringboot/RabbitmqSpringbootProducerApplication.java
@@ -0,0 +1,18 @@
+package com.yanghx.rabbitmqspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * springboot整合rabbitMQ 生产端
+ *
+ * @author yangheng
+ */
+@SpringBootApplication
+public class RabbitmqSpringbootProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitmqSpringbootProducerApplication.class, args);
+    }
+
+}
